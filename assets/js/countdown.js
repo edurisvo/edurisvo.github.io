8e2f0783451990b261bf42e7689debcb1dd3aae3
Federico Monaco <FedericoMonaco@users.noreply.github.com>
2022-08-07 16:27:05 +0200

    Update countdown.js

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -19,7 +19,7 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
   // Output the results
-  document.getElementById("countdown").innerHTML="EDURISVO partecipa a... tra<br><b>" + days + "</b> giorni <b>" + hours + "</b> ore e <b>"
+  document.getElementById("countdown").innerHTML="EDURISVO inizia tra<br><b>" + days + "</b> giorni <b>" + hours + "</b> ore e <b>"
   + minutes + "</b> minuti";
 
   //If the count down is over, write some text 
